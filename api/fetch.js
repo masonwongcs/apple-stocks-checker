5ad83842d4fe80c3c114b03d3f1eafe820e01b4d
Mason Wong <mason@orion.co.com>
2020-11-09 21:45:59 +0800

    Update api

--- api/fetch.js
+++ api/fetch.js
@@ -10,60 +10,77 @@ const to = process.env.TO;
 // MHLQ3ZA/A
 
 const sendMail = (message) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: username,
-      pass: password,
-    },
-  });
+  return new Promise((resolve) => {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: username,
+        pass: password,
+      },
+    });
 
-  const mailOptions = {
-    from: to,
-    to: to,
-    subject: "Apple Leather Case",
-    text: message,
-  };
+    const mailOptions = {
+      from: to,
+      to: to,
+      subject: "Apple Leather Case",
+      text: message,
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        resolve({
+          error: true,
+          message: error,
+        });
+      } else {
+        console.log("Email sent: " + info.response);
+        resolve({
+          error: false,
+          message: "Email sent: " + info.response,
+        });
+      }
+    });
   });
 };
 
 const getAppleStocksDetails = (partID) => {
-  axios
-    .get(
-      `https://www.apple.com/sg/shop/retail/pickup-message?parts.0=${partID}&searchNearby=true&store=R669`
-    )
-    .then((response) => {
-      const data = response.data.body;
-      const { stores } = data;
+  return new Promise((resolve) => {
+    axios
+      .get(
+        `https://www.apple.com/sg/shop/retail/pickup-message?parts.0=${partID}&searchNearby=true&store=R669`
+      )
+      .then(async (response) => {
+        const data = response.data.body;
+        const { stores } = data;
 
-      const isAvailable = stores.find(
-        (item) => item.partsAvailability[partID].storeSelectionEnabled
-      );
+        const isAvailable = stores.find(
+          (item) => item.partsAvailability[partID].storeSelectionEnabled
+        );
 
-      if (isAvailable) {
-        const message = `${isAvailable.storeName} have stocks now.`;
-        sendMail(message);
-        console.log(isAvailable.storeName);
-        return isAvailable.storeName;
-      } else {
-        console.log("No Stock");
-        return "No Stock";
-      }
-    });
+        if (isAvailable) {
+          const message = `${isAvailable.storeName} have stocks now.`;
+          const { error, message: emailMessage } = await sendMail(message);
+          console.log(isAvailable.storeName);
+          if (error) {
+            resolve(emailMessage);
+          } else {
+            resolve(`${isAvailable.storeName}, ${emailMessage}`);
+          }
+        } else {
+          console.log("No Stock");
+          resolve("No Stock");
+        }
+      });
+  });
 };
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const partID = req.query.partID;
-  const response = getAppleStocksDetails(partID);
+  const response = await getAppleStocksDetails(partID);
 
+  console.log(response);
   res.json({
-    message: response
-  })
+    message: response,
+  });
 };
